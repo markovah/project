4c279838ad9d77b68fb0577bf37d5c2ea60b942e
unknown <markovah@VW-ANDROID-01.pk.amk.root>
2014-11-04 15:41:33 +0200

    Signed-off-by: unknown <markovah@VW-ANDROID-01.pk.amk.root>

--- 1.js
+++
@@ -1,8 +0,0 @@
-function showDog() {
-  document.getElementsByTagName('img')[0].setAttribute('src', 'dog.jpg');
-}
-
-function showCat() {
-  document.getElementsByTagName('img')[0].setAttribute('src', 'cat.jpg');
-}
-
